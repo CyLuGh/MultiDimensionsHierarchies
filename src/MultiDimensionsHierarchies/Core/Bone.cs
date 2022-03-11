5e4c4cdb8f185719994f3049926888b2cd0d7ca1
Jeremy Demortier <jeremy.demortier@gmail.com>
2022-03-11 11:46:50 +0100

    First commit

---
+++ src/MultiDimensionsHierarchies/Core/Bone.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiDimensionsHierarchies.Core
+{
+    public class Bone
+    {
+    }
+}
---
+++ src/MultiDimensionsHierarchies/Core/Dimension.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiDimensionsHierarchies.Core
+{
+    public class Dimension
+    {
+        public string Name { get; init; }
+    }
+}
