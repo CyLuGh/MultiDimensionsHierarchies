0208219fc431bf6918e36c5ef4c9dfa5603f79f9
Jeremy Demortier <jeremy.demortier@gmail.com>
2022-03-11 20:09:58 +0100

    Make Bone immutable

--- src/MultiDimensionsHierarchies/Core/Bone.cs
+++ src/MultiDimensionsHierarchies/Core/Bone.cs
@@ -1,10 +1,90 @@
-﻿using System;
+﻿using LanguageExt;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MultiDimensionsHierarchies.Core
 {
-    public class Bone
+    public class Bone : IEquatable<Bone>
     {
+        public static Bone None { get; } = new Bone( "None" , "?" );
+
+        public string DimensionName { get; }
+        public string Label { get; }
+        public Option<Bone> Parent { get; }
+        public Seq<Bone> Children { get; }
+
+        public List<Bone> Leaves
+        {
+            get
+            {
+                var leaves = FetchLeaves().Memo();
+                return leaves();
+            }
+        }
+
+        public Bone( string label , string dimensionName )
+        {
+            DimensionName = dimensionName;
+            Label = label;
+            Parent = Option<Bone>.None;
+            Children = new Seq<Bone>();
+        }
+
+        public Bone( string label , string dimensionName , Option<Bone> parent )
+            : this( label , dimensionName )
+        {
+            Parent = parent;
+        }
+
+        public Bone( string label , string dimensionName , params Bone[] children )
+            : this( label , dimensionName )
+        {
+            Children = new Seq<Bone>( children.Select( c => c.With( parent: this ) ) );
+        }
+
+        public Bone With( string label = null , string dimensionName = null , Bone parent = null )
+            => new Bone(
+                    label ?? this.Label ,
+                    dimensionName ?? this.DimensionName ,
+                    parent ?? this.Parent
+                );
+
+        public bool IsLeaf() => Children.Count == 0;
+
+        public bool HasChild() => Children.Count > 0;
+
+        public bool HasParent() => Parent.IsSome;
+
+        public Bone GetRoot()
+            => Parent.Some( p => p )
+                .None( () => this );
+
+        private Func<List<Bone>> FetchLeaves()
+            => () =>
+            {
+                if ( HasChild() )
+                    return Children.SelectMany( child => child.Leaves ).ToList();
+
+                return new List<Bone> { this };
+            };
+
+        public bool Equals( Bone other )
+        {
+            if ( ReferenceEquals( null , other ) ) return false;
+            if ( ReferenceEquals( this , other ) ) return true;
+            return string.Equals( Label , other.Label )
+                && string.Equals( DimensionName , other.DimensionName )
+                && Parent.Equals( other.Parent );
+        }
+
+        public override bool Equals( object obj )
+        {
+            if ( ReferenceEquals( null , obj ) ) return false;
+            if ( ReferenceEquals( this , obj ) ) return true;
+            if ( obj.GetType() != this.GetType() ) return false;
+            return Equals( (Bone) obj );
+        }
     }
 }
--- src/TestMultiDimensionsHierarchies/BoneTests.cs
+++ src/TestMultiDimensionsHierarchies/BoneTests.cs
@@ -1,11 +1,20 @@
+using FluentAssertions;
+using MultiDimensionsHierarchies.Core;
 using Xunit;
 
 namespace TestMultiDimensionsHierarchies;
-public class UnitTest1
+
+public class BoneTests
 {
     [Fact]
-    public void Test1()
+    public void TestChildren()
     {
+        var child1 = new Bone( "Child 1" , "Test dimension" );
+        var child2 = new Bone( "Child 2" , "Test dimension" );
 
+        var parent = new Bone( "Parent" , "Test dimension" , child1 , child2 );
+
+        parent.HasChild().Should().BeTrue();
+        parent.Children.Length.Should().Be( 2 );
     }
 }
