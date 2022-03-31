e9ff12310eb330778eabf8a5e6d4a15bc303383b
Demortier Jeremy <jeremy.demortier@nbb.be>
2022-03-31 10:35:33 +0200

    Fix style issue

--- src/Benchmark/HierarchyInput.cs
+++ src/Benchmark/HierarchyInput.cs
@@ -1,29 +1,12 @@
-﻿using System;
-
-namespace Benchmark
+﻿namespace Benchmark
 {
-    public abstract class HierarchyInput<T>
+    public class ParentHierarchyInput<T>
     {
         public T Id { get; set; }
+        public T ParentId { get; set; }
         public string Label { get; set; }
 
         public override string ToString()
             => $"{Id} {Label}";
-    }
-
-    public class ParentHierarchyInput<T> : HierarchyInput<T>
-    {
-        public T ParentId { get; set; }
-    }
-
-    public class ChildHierarchyInput<T> : HierarchyInput<T>
-    {
-        public T ChildId { get; set; }
-    }
-
-    public class MultiChildrenHierarchyInput<T> : HierarchyInput<T>
-    {
-        public T[] ChildrenIds { get; set; }
-            = Array.Empty<T>();
     }
 }
