87385a91862cdcf4ac406e97c037d9d79b6d8cd0
Jeremy Demortier <64773074+CyLuGh@users.noreply.github.com>
2022-06-24 11:36:47 +0200

    Add TryBuild methods to SkeletonFactory
    TryBuild methods won't throw any exception if a skeleton can't be built but will return an Error.

--- src/MultiDimensionsHierarchies/Core/SkeletonFactory.cs
+++ src/MultiDimensionsHierarchies/Core/SkeletonFactory.cs
@@ -1,4 +1,5 @@
 ﻿using LanguageExt;
+using LanguageExt.Common;
 using MoreLinq;
 using System;
 using System.Collections.Generic;
@@ -17,6 +18,8 @@ namespace MultiDimensionsHierarchies.Core
         /// <param name="dimensions">Dimensions with their hierarchies</param>
         /// <param name="dimensionsOfInterest">(Optional) Subset of dimensions to be used</param>
         /// <returns>Sequence of properly defined Skeletons</returns>
+        /// <exception cref="ArgumentException">Throws if a dimension of interest is not included in dimensions</exception>
+        /// <exception cref="ApplicationException">Throws if skeleton creation can't find a required value in dimensions</exception>
         public static Seq<Skeleton> BuildSkeletons<T>( IEnumerable<T> inputs ,
             Func<T , string , string> parser ,
             IEnumerable<Dimension> dimensions ,
@@ -36,6 +39,8 @@ namespace MultiDimensionsHierarchies.Core
         /// <param name="dimensions">Dimensions with their hierarchies</param>
         /// <param name="dimensionsOfInterest">(Optional) Subset of dimensions to be used</param>
         /// <returns>Sequence of properly defined Skeletons, containing data</returns>
+        /// <exception cref="ArgumentException">Throws if a dimension of interest is not included in dimensions</exception>
+        /// <exception cref="ApplicationException">Throws if skeleton creation can't find a required value in dimensions</exception>
         public static Seq<Skeleton<T>> BuildSkeletons<T, TI>( IEnumerable<TI> inputs ,
           Func<TI , string , string> parser ,
           Func<TI , T> evaluator ,
@@ -54,6 +59,8 @@ namespace MultiDimensionsHierarchies.Core
         /// <param name="dimensions">Dimensions with their hierarchies</param>
         /// <param name="dimensionsOfInterest">(Optional) Subset of dimensions to be used</param>
         /// <returns>Sequence of properly defined Skeletons</returns>
+        /// <exception cref="ArgumentException">Throws if a dimension of interest is not included in dimensions</exception>
+        /// <exception cref="ApplicationException">Throws if skeleton creation can't find a required value in dimensions</exception>
         public static Seq<Skeleton> BuildSkeletons( IEnumerable<string> stringInputs ,
             Func<string , string[]> partitioner ,
             Func<string[] , string , string> selectioner ,
@@ -64,12 +71,78 @@ namespace MultiDimensionsHierarchies.Core
                 selectioner , dimensions , dimensionsOfInterest );
         }
 
+        /// <summary>
+        /// Build skeletons from source items.
+        /// </summary>
+        /// <typeparam name="T">Type of source data</typeparam>
+        /// <param name="inputs">Source data</param>
+        /// <param name="parser">How to find defined bone in dimension in source item</param>
+        /// <param name="dimensions">Dimensions with their hierarchies</param>
+        /// <param name="dimensionsOfInterest">(Optional) Subset of dimensions to be used</param>
+        /// <returns>Sequence of properly defined Skeletons</returns>
+        /// <exception cref="ArgumentException">Throws if a dimension of interest is not included in dimensions</exception>
+        public static Seq<Either<Error , Skeleton>> TryBuildSkeletons<T>( IEnumerable<T> inputs ,
+            Func<T , string , string> parser ,
+            IEnumerable<Dimension> dimensions ,
+            string[] dimensionsOfInterest = null )
+        {
+            return TryCreateSkeletons( inputs , parser , dimensions , dimensionsOfInterest ).ToSeq();
+        }
+
+        /// <summary>
+        /// Build skeletons with their associated value from source items.
+        /// </summary>
+        /// <typeparam name="T">Type of source data</typeparam>
+        /// <typeparam name="TI">Type of value associated to skeleton</typeparam>
+        /// <param name="inputs">Source data</param>
+        /// <param name="parser">How to find defined bone in dimension in source item</param>
+        /// <param name="evaluator">How to find associated value in source item</param>
+        /// <param name="dimensions">Dimensions with their hierarchies</param>
+        /// <param name="dimensionsOfInterest">(Optional) Subset of dimensions to be used</param>
+        /// <returns>Sequence of properly defined Skeletons, containing data</returns>
+        /// <exception cref="ArgumentException">Throws if a dimension of interest is not included in dimensions</exception>
+        public static Seq<Either<Error , Skeleton<T>>> TryBuildSkeletons<T, TI>( IEnumerable<TI> inputs ,
+          Func<TI , string , string> parser ,
+          Func<TI , T> evaluator ,
+          IEnumerable<Dimension> dimensions ,
+          string[] dimensionsOfInterest = null )
+        {
+            return TryCreateSkeletons( inputs , parser , evaluator , dimensions , dimensionsOfInterest ).ToSeq();
+        }
+
+        /// <summary>
+        /// Build skeletons from string sources
+        /// </summary>
+        /// <param name="stringInputs">Inputs, in string format</param>
+        /// <param name="partitioner">How to split the input string</param>
+        /// <param name="selectioner">How to find needed dimension in input string parts</param>
+        /// <param name="dimensions">Dimensions with their hierarchies</param>
+        /// <param name="dimensionsOfInterest">(Optional) Subset of dimensions to be used</param>
+        /// <returns>Sequence of properly defined Skeletons</returns>
+        /// <exception cref="ArgumentException">Throws if a dimension of interest is not included in dimensions</exception>
+        public static Seq<Either<Error , Skeleton>> TryBuildSkeletons( IEnumerable<string> stringInputs ,
+            Func<string , string[]> partitioner ,
+            Func<string[] , string , string> selectioner ,
+            IEnumerable<Dimension> dimensions ,
+            string[] dimensionsOfInterest = null )
+        {
+            return TryBuildSkeletons( stringInputs.Select( stringInput => partitioner( stringInput ) ) ,
+                selectioner , dimensions , dimensionsOfInterest );
+        }
+
         internal static IEnumerable<Skeleton> CreateSkeletons<T>( IEnumerable<T> inputs ,
             Func<T , string , string> parser ,
             IEnumerable<Dimension> dimensions ,
             string[] dimensionsOfInterest = null )
             => CreateSkeletons( inputs , parser , _ => Unit.Default , dimensions , dimensionsOfInterest )
                 .Select( s => s.Key );
+
+        internal static IEnumerable<Either<Error , Skeleton>> TryCreateSkeletons<T>( IEnumerable<T> inputs ,
+            Func<T , string , string> parser ,
+            IEnumerable<Dimension> dimensions ,
+            string[] dimensionsOfInterest = null )
+            => TryCreateSkeletons( inputs , parser , _ => Unit.Default , dimensions , dimensionsOfInterest )
+                .Map( x => x.Map( s => s.Key ) );
 
         internal static IEnumerable<Skeleton<U>> CreateSkeletons<T, U>( IEnumerable<T> inputs ,
             Func<T , string , string> parser ,
@@ -113,6 +186,59 @@ namespace MultiDimensionsHierarchies.Core
                         ( skels , bs ) => skels.Cartesian( bs , ( s , b ) => s.Add( b ) ) )
                    .Select( skel => new Skeleton<U>( evaluator( input ) , skel ) );
             } );
+        }
+
+        internal static IEnumerable<Either<Error , Skeleton<U>>> TryCreateSkeletons<T, U>( IEnumerable<T> inputs ,
+            Func<T , string , string> parser ,
+            Func<T , U> evaluator ,
+            IEnumerable<Dimension> dimensions ,
+            string[] dimensionsOfInterest = null )
+        {
+            var seqDimensions = dimensions.ToSeq();
+
+            if ( dimensionsOfInterest != null )
+            {
+                var missings = dimensionsOfInterest
+                .Select( d => seqDimensions.Find( o => o.Name.Equals( d ) )
+                    .Some( _ => string.Empty ).None( () => d ) ).Where( s => !string.IsNullOrEmpty( s ) )
+                .ToSeq();
+
+                if ( missings.Any() )
+                {
+                    var missing = string.Join( ", " , missings );
+                    throw new ArgumentException( $"Some dimensions of interest aren't available in the dimensions collection: {missing}." );
+                }
+            }
+            else
+            {
+                dimensionsOfInterest = seqDimensions.Select( d => d.Name ).ToArray();
+            }
+
+            return inputs.AsParallel().SelectMany( input =>
+            {
+                var bones = dimensionsOfInterest.Select( d => FindBones( input , parser , d , seqDimensions ) )
+                    .ToArray();
+                return TryCreateSkeletons( input , evaluator , bones );
+            } );
+        }
+
+        internal static IEnumerable<Either<Error , Skeleton<U>>> TryCreateSkeletons<T, U>( T input , Func<T , U> evaluator , Either<string , Bone[]>[] bones )
+        {
+            if ( bones.Lefts().Any() )
+            {
+                var missing = string.Join( ", " , bones.Lefts() );
+                yield return Error.New( $"Couldn't find some dimensions values for {input}: {missing}" );
+            }
+            else
+            {
+                foreach ( var s in bones.Rights()
+                    .Aggregate<IEnumerable<Bone> , IEnumerable<Skeleton>>( new[] { new Skeleton() } ,
+                        ( skels , bs ) => skels.Cartesian( bs , ( s , b ) => s.Add( b ) ) )
+                   .Select( skel => new Skeleton<U>( evaluator( input ) , skel ) ) )
+                {
+                    yield return s;
+                }
+            }
         }
 
         internal static Either<string , Bone[]> FindBones<T>( T input , Func<T , string , string> parser , string dimensionName , Seq<Dimension> dimensions )
--- src/TestMultiDimensionsHierarchies/SkeletonFactoryTests.cs
+++ src/TestMultiDimensionsHierarchies/SkeletonFactoryTests.cs
@@ -2,6 +2,7 @@
 using LanguageExt.UnitTesting;
 using MultiDimensionsHierarchies.Core;
 using System;
+using System.Linq;
 using Xunit;
 
 namespace TestMultiDimensionsHierarchies;
@@ -40,6 +41,40 @@ public class SkeletonFactoryTests
     }
 
     [Fact]
+    public void TestTryBuild()
+    {
+        var dimA = SkeletonTests.GetDimension( "Dimension A" );
+        var dimB = SkeletonTests.GetDimension( "Dimension B" );
+
+        var items = new[] { (DimA: "1.1", dimB: "2") , (DimA: "1.2", dimB: "2.3") };
+        var parser = ( (string DimA, string DimB) t , string dim ) => dim switch
+        {
+            "Dimension A" => t.DimA,
+            "Dimension B" => t.DimB,
+            _ => string.Empty,
+        };
+
+        var skeletons = SkeletonFactory.TryBuildSkeletons( items , parser , new[] { dimA , dimB } , new[] { "Dimension A" , "Dimension B" } );
+        skeletons.Length.Should().Be( 2 );
+
+        skeletons[0]
+            .ShouldBeRight( s => s.Bones.Length.Should().Be( 2 ) );
+        skeletons.Rights()
+            .Find( ("1.1", "Dimension A") , ("2", "Dimension B") )
+            .ShouldBeSome( s => dimA.Find( "1.1" ).ShouldBeSome( d => s.Bones[0].Should().BeSameAs( d ) ) );
+
+        var skeletons2 = SkeletonFactory.TryBuildSkeletons( items , parser , new[] { dimA , dimB } );
+        skeletons2.Length.Should().Be( 2 );
+
+        skeletons2[0]
+            .ShouldBeRight( s => s.Bones.Length.Should().Be( 2 ) );
+        skeletons2.Rights().Find( ("1.1", "Dimension A") , ("2", "Dimension B") )
+            .ShouldBeSome( s => dimA.Find( "1.1" ).ShouldBeSome( d => s.Bones[0].Should().BeSameAs( d ) ) );
+
+        skeletons.Should().BeEquivalentTo( skeletons2 );
+    }
+
+    [Fact]
     public void TestMissingDimension()
     {
         var dimA = SkeletonTests.GetDimension( "Dimension A" );
@@ -63,6 +98,29 @@ public class SkeletonFactoryTests
     }
 
     [Fact]
+    public void TestTryMissingDimension()
+    {
+        var dimA = SkeletonTests.GetDimension( "Dimension A" );
+        var dimB = SkeletonTests.GetDimension( "Dimension B" );
+
+        var items = new[] { (DimA: "1.1", dimB: "2") , (DimA: "1.2", dimB: "2.3") };
+        var parser = ( (string DimA, string DimB) t , string dim ) => dim switch
+        {
+            "Dimension A" => t.DimA,
+            "Dimension B" => t.DimB,
+            _ => string.Empty,
+        };
+
+        var act = () => SkeletonFactory.TryBuildSkeletons( items , parser , new[] { dimA , dimB } , new[] { "Dimension A" , "Dimension C" } );
+        act.Should().Throw<ArgumentException>()
+            .WithMessage( "Some dimensions of interest aren't available in the dimensions collection: Dimension C." );
+
+        act = () => SkeletonFactory.TryBuildSkeletons( items , parser , new[] { dimA , dimB } , new[] { "Dimension A" , "Dimension C" , "Dimension D" } );
+        act.Should().Throw<ArgumentException>()
+            .WithMessage( "Some dimensions of interest aren't available in the dimensions collection: Dimension C, Dimension D." );
+    }
+
+    [Fact]
     public void TestMissingValue()
     {
         var dimA = SkeletonTests.GetDimension( "Dimension A" );
@@ -79,6 +137,31 @@ public class SkeletonFactoryTests
         var act = () => SkeletonFactory.BuildSkeletons( items , parser , new[] { dimA , dimB } , new[] { "Dimension A" , "Dimension B" } ).ToArray();
         act.Should().Throw<ApplicationException>()
                 .WithMessage( "Couldn't find some dimensions values: Couldn't find 4 in dimension Dimension B" );
+    }
+
+    [Fact]
+    public void TestTryMissingValue()
+    {
+        var dimA = SkeletonTests.GetDimension( "Dimension A" );
+        var dimB = SkeletonTests.GetDimension( "Dimension B" );
+
+        var items = new[] { (DimA: "1.1", dimB: "2") , (DimA: "1.2", dimB: "4") };
+        var parser = ( (string DimA, string DimB) t , string dim ) => dim switch
+        {
+            "Dimension A" => t.DimA,
+            "Dimension B" => t.DimB,
+            _ => string.Empty,
+        };
+
+        var act = () => SkeletonFactory.TryBuildSkeletons( items , parser , new[] { dimA , dimB } , new[] { "Dimension A" , "Dimension B" } ).ToArray();
+        act.Should().NotThrow<ApplicationException>();
+
+        var skeletons = SkeletonFactory.TryBuildSkeletons( items , parser , new[] { dimA , dimB } , new[] { "Dimension A" , "Dimension B" } ).ToArray();
+        skeletons.Rights().Should().HaveCount( 1 );
+        skeletons.Lefts().Should().HaveCount( 1 );
+
+        skeletons.Lefts().First()
+            .Message.Should().Be( "Couldn't find some dimensions values for (1.2, 4): Couldn't find 4 in dimension Dimension B" );
     }
 
     [Fact]
@@ -109,6 +192,34 @@ public class SkeletonFactoryTests
     }
 
     [Fact]
+    public void TestTryStringBuild()
+    {
+        var dimA = SkeletonTests.GetDimension( "Dimension A" );
+        var dimB = SkeletonTests.GetDimension( "Dimension B" );
+        var dimC = SkeletonTests.GetDimension( "Dimension C" );
+
+        var items = new[] { "1.1:2.1:1.1.1" , "1.2:2.4:2.1" };
+        var partitioner = ( string s ) => s.Split( ':' );
+        var selectioner = ( string[] parts , string dim )
+            => dim switch
+            {
+                "Dimension A" => parts[1],
+                "Dimension B" => parts[2],
+                "Dimension C" => parts[0],
+                _ => string.Empty,
+            };
+
+        var skeletons = SkeletonFactory.TryBuildSkeletons( items , partitioner , selectioner , new[] { dimA , dimB , dimC } );
+        skeletons.Length.Should().Be( 2 );
+
+        skeletons[0]
+            .ShouldBeRight( s => s.Bones.Length.Should().Be( 3 ) );
+
+        skeletons.Rights().Find( "2.1" , "1.1.1" , "1.1" )
+            .ShouldBeSome( s => dimA.Find( "2.1" ).ShouldBeSome( d => s.Bones[0].Should().BeSameAs( d ) ) );
+    }
+
+    [Fact]
     public void TestRedundantItems()
     {
         var dimA = SkeletonTests.GetDimensionWithRedundantLabel( "Dimension A" );
@@ -135,6 +246,32 @@ public class SkeletonFactoryTests
     }
 
     [Fact]
+    public void TestTryRedundantItems()
+    {
+        var dimA = SkeletonTests.GetDimensionWithRedundantLabel( "Dimension A" );
+        var dimB = SkeletonTests.GetDimensionWithRedundantLabel( "Dimension B" );
+        var dimC = SkeletonTests.GetDimensionWithRedundantLabel( "Dimension C" );
+
+        var items = new[] { "1.1:2.1:1.1.1" , "1.2:2.4:2.1" };
+        var partitioner = ( string s ) => s.Split( ':' );
+        var selectioner = ( string[] parts , string dim )
+            => dim switch
+            {
+                "Dimension A" => parts[1],
+                "Dimension B" => parts[2],
+                "Dimension C" => parts[0],
+                _ => string.Empty,
+            };
+
+        var skeletons = SkeletonFactory.TryBuildSkeletons( items , partitioner , selectioner , new[] { dimA , dimB , dimC } );
+        skeletons.Length.Should().Be( 2 );
+
+        items = new[] { "1.1:0:1.1.1" , "1.2:2.4:2.1" };
+        skeletons = SkeletonFactory.TryBuildSkeletons( items , partitioner , selectioner , new[] { dimA , dimB , dimC } );
+        skeletons.Length.Should().Be( 3 );
+    }
+
+    [Fact]
     public void TestBuildGeneric()
     {
         var dimA = SkeletonTests.GetDimension( "Dimension A" );
@@ -154,6 +291,35 @@ public class SkeletonFactoryTests
 
         skeletons.All( s => s.Bones.Length == 2 ).Should().BeTrue();
         skeletons.Find( "1.1" , "2" )
+            .ShouldBeSome( s =>
+            {
+                dimA.Find( "1.1" ).ShouldBeSome( d => s.Bones[0].Should().BeSameAs( d ) );
+
+                s.Value.ShouldBeSome( v => v.Should().Be( 4.6 ) );
+                s.ValueUnsafe.Should().Be( 4.6 );
+            } );
+    }
+
+    [Fact]
+    public void TestTryBuildGeneric()
+    {
+        var dimA = SkeletonTests.GetDimension( "Dimension A" );
+        var dimB = SkeletonTests.GetDimension( "Dimension B" );
+
+        var items = new[] { (DimA: "1.1", DimB: "2", Value: 4.6) , (DimA: "1.2", DimB: "2.3", Value: 7d) };
+        var parser = ( (string DimA, string DimB, double _) t , string dim ) => dim switch
+        {
+            "Dimension A" => t.DimA,
+            "Dimension B" => t.DimB,
+            _ => string.Empty,
+        };
+        var evaluator = ( (string DimA, string DimB, double Value) t ) => t.Value;
+
+        var skeletons = SkeletonFactory.TryBuildSkeletons( items , parser , evaluator , new[] { dimA , dimB } , new[] { "Dimension A" , "Dimension B" } );
+        skeletons.Length.Should().Be( 2 );
+
+        skeletons.Rights().All( s => s.Bones.Length == 2 ).Should().BeTrue();
+        skeletons.Rights().Find( "1.1" , "2" )
             .ShouldBeSome( s =>
             {
                 dimA.Find( "1.1" ).ShouldBeSome( d => s.Bones[0].Should().BeSameAs( d ) );
